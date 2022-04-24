893d1640b11aabb274dc6d648486e8270dba8ca9
abaradarantutunchi <abaradarantutunchi@ebay.com>
2022-04-24 21:27:17 +0200

    move feed loader to production

--- EssentialFeed/FeedFeature/FeedLoader.swift
+++ EssentialFeed/FeedFeature/FeedLoader.swift
@@ -6,18 +6,3 @@ enum LoadFeedResult {
 protocol FeedLoader {
     func load(completion: (LoadFeedResult) -> Void)
 }
-
-class RemoteFeedLoader {
-    let client: HTTPClient
-    let url: URL
-    init(url: URL, client: HTTPClient) {
-        self.client = client
-        self.url = url
-    }
-    func load() {
-        client.get(from: url)
-    }
-}
-protocol HTTPClient {
-    func get(from url: URL)
-}
