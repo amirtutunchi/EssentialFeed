b719c874762cf931468e0e7d67e527db9288cd5e
Amirhossein BaradaranTutunchi <amirhossein.baradarantutunchi@adevinta.com>
2022-07-30 10:44:39 +0200

    replace RemoteFeedLoadder.Result with FeedLoader result in preparation to conform to it protocol

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -12,10 +12,9 @@ public final class RemoteFeedLoader {
         case connectivity
         case invalidData
     }
-    public enum Result: Equatable {
-        case success([FeedItem])
-        case failure(Error)
-    }
+    
+    public typealias Result = LoadFeedResult<Error>
+    
     public func load(completion: @escaping (Result) -> Void) {
         client.get(from: url) { [weak self] result in
             guard self != nil else { return }
