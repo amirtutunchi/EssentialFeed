4e495a13a57bc9f2172bc24dac098c56ea8ac820
Amirhossein BaradaranTutunchi <amirhossein.baradarantutunchi@adevinta.com>
2022-08-03 17:55:46 +0200

    add memory leak tracking

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -16,11 +16,13 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
     
     //MARK: - Helpers:
     
-    private func getFeedResult() -> LoadFeedResult? {
+    private func getFeedResult(file: StaticString = #file, line: UInt = #line) -> LoadFeedResult? {
         let url = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5c52cdd0b8a045df091d2fff/1548930512083/feed-case-study-test-api-feed.json")!
         let client = URLSessionHTTPClient()
         let loader = RemoteFeedLoader(url: url, client: client)
         var receivedResult: LoadFeedResult?
+        addTrackForMemoryLeak(object: client,file: file, line: line)
+        addTrackForMemoryLeak(object: loader)
         let exp = expectation(description: "Wait for execution...")
         loader.load { result in
             receivedResult = result
