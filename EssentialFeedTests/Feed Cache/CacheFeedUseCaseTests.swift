43974bf70ebefd894b042ba19f9052e44ed81e7b
Amirhossein BaradaranTutunchi <amirhossein.baradarantutunchi@adevinta.com>
2022-08-05 16:47:32 +0200

    does not insert of store deletion error

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -13,9 +13,14 @@ class LocalFeedLoader {
 class FeedStore {
     private var deleteCacheFeedCallCount = 0
     var deleteCacheCount: Int { deleteCacheFeedCallCount }
+    private var insertCacheFeedCallCount = 0
+    var insertionCount: Int { insertCacheFeedCallCount }
     
     func deleteCache() {
         deleteCacheFeedCallCount += 1
+    }
+    func completeDeletion(with error: Error) {
+        
     }
 }
 class CacheFeedUseCaseTests: XCTestCase {
@@ -31,6 +36,15 @@ class CacheFeedUseCaseTests: XCTestCase {
         sut.save(item: items)
         XCTAssertEqual(store.deleteCacheCount, 1)
     }
+    
+    func test_save_doesNotInsertCacheOnCacheDeletionError() {
+        let (sut, store) = makeSUT()
+        let items = [UniqueItem(), UniqueItem()]
+        sut.save(item: items)
+        let error = anyError()
+        store.completeDeletion(with: error)
+        XCTAssertEqual(store.insertionCount, 0)
+    }
 }
 
 //MARK: - Helpers
@@ -45,7 +59,15 @@ extension CacheFeedUseCaseTests {
     }
     
     private func UniqueItem() -> FeedItem {
-        FeedItem(id: UUID(), description: "desc", location: "", imageURL: URL(string: "http://anyurl.com")!)
+        FeedItem(id: UUID(), description: "desc", location: "", imageURL: anyURL())
+    }
+    
+    private func anyURL() -> URL {
+        URL(string: "http://a-url.com")!
+    }
+    
+    private func anyError() -> NSError {
+        NSError(domain: "any error", code: 0)
     }
 }
 #endif
