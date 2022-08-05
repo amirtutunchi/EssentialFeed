1a9fdacc35c6bb6d872674f1e3f50df97e16e9f2
Amirhossein BaradaranTutunchi <amirhossein.baradarantutunchi@adevinta.com>
2022-08-05 16:24:04 +0200

    delete cache after calling Save on 'LocalFeedLoader'

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -1,16 +1,21 @@
 import XCTest
 
 class LocalFeedLoader {
-    var store: FeedStore
+    private let store: FeedStore
     init(store: FeedStore) {
         self.store = store
+    }
+    func save() {
+        store.deleteCache()
     }
 }
 
 class FeedStore {
-    var numberOfDeleteCaches = 0
-    init() {
-        
+    private var deleteCacheFeedCallCount = 0
+    var deleteCacheCount: Int { deleteCacheFeedCallCount }
+    
+    func deleteCache() {
+        deleteCacheFeedCallCount += 1
     }
 }
 class CacheFeedUseCaseTests: XCTestCase {
@@ -18,6 +23,13 @@ class CacheFeedUseCaseTests: XCTestCase {
     func test_init_doesNotDeleteCacheUponCreation() {
         let store = FeedStore()
         let _ = LocalFeedLoader(store: store)
-        XCTAssertEqual(store.numberOfDeleteCaches, 0)
+        XCTAssertEqual(store.deleteCacheCount, 0)
+    }
+    
+    func test_save_deleteCache() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        sut.save()
+        XCTAssertEqual(store.deleteCacheCount, 1)
     }
 }
