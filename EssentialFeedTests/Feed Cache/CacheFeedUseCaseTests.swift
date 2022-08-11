608cf4eaedb510be509e83a58fcd0dfdbd52fab4
Amirhossein BaradaranTutunchi <amirhossein.baradarantutunchi@adevinta.com>
2022-08-11 23:36:37 +0200

    Move FeeStoreSpy to its own file

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -123,39 +123,5 @@ extension CacheFeedUseCaseTests {
     private func anyError() -> NSError {
         NSError(domain: "any error", code: 0)
     }
-    
-    private class FeedStoreSpy: FeedStore {
-        typealias DeletionCompletion = (Error?) -> Void
-        typealias InsertionCompletion = (Error?) -> Void
-        enum Message: Equatable {
-            case delete
-            case insert([LocalFeedImage], Date)
-        }
-        
-        private(set) var messages = [Message]()
-        private var deletionCompletion = [DeletionCompletion]()
-        private var insertionCompletion = [InsertionCompletion]()
-        func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-            deletionCompletion.append(completion)
-            messages.append(.delete)
-        }
-        func completeDeletion(with error: Error, index: Int = 0) {
-            deletionCompletion[index](error)
-        }
-        func completeDeletionSuccessfully(index: Int = 0) {
-            deletionCompletion[index](nil)
-        }
-        func insertCache(items: [LocalFeedImage], timeStamp: Date, completion: @escaping InsertionCompletion) {
-            insertionCompletion.append(completion)
-            messages.append(.insert(items, timeStamp))
-        }
-        func completeInsertion(with error: Error, index: Int = 0) {
-            insertionCompletion[index](error)
-        }
-
-        func completeInsertionSuccessfully(index: Int = 0) {
-            insertionCompletion[index](nil)
-        }
-    }
 }
 #endif
