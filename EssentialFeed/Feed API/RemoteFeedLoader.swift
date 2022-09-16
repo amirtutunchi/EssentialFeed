ac1cf14fb95d158f2a33c00f936ae0c9d8abf217
Amir Hossein Baradaran Toutounchi <amirhossein.baradarantutunchi@adevinta.com>
2022-09-16 14:37:55 +0200

    refactor "FeedLoader.Result"

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -13,7 +13,7 @@ public final class RemoteFeedLoader: FeedLoader {
         case invalidData
     }
     
-    public typealias Result = LoadFeedResult
+    public typealias Result = FeedLoader.Result
     
     public func load(completion: @escaping (Result) -> Void) {
         client.get(url: url) { [weak self] result in
