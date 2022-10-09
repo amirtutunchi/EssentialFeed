4670e46668ac56a0cafa5a6372804e75f9423256
Amir Hossein Baradaran Toutounchi <amirhossein.baradarantutunchi@adevinta.com>
2022-10-09 10:24:40 +0200

    move production code to production

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -1,29 +1,6 @@
 import XCTest
-import UIKit
+import EssentialFeediOS
 import EssentialFeed
-
-class FeedViewController: UITableViewController {
-    private var loader: FeedLoader?
-    
-    convenience init(loader: FeedLoader) {
-        self.init()
-        self.loader = loader
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        refreshControl = UIRefreshControl()
-        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
-        load()
-    }
-    
-    @objc
-    private func load() {
-        refreshControl?.beginRefreshing()
-        loader?.load {[weak self] _ in
-            self?.refreshControl?.endRefreshing()
-        }
-    }
-}
 
 class FeedViewControllerTests: XCTestCase {
     
