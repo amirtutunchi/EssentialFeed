f372ec1c5d0395f8310c43b5fc4490be673b7ba9
Amir Hossein Baradaran Toutounchi <amirhossein.baradarantutunchi@adevinta.com>
2022-12-04 14:24:34 +0100

    refactor test helpers implement "didStartLoadingFeed"

--- EssentialFeedTests/Feed Presenter/FeedPresenterTests.swift
+++ EssentialFeedTests/Feed Presenter/FeedPresenterTests.swift
@@ -1,19 +1,61 @@
 import XCTest
 
+struct FeedLoadingViewModel: Equatable {
+    let isLoading: Bool
+}
+protocol FeedLoadingView {
+    func loadingStateChanged(viewModel: FeedLoadingViewModel)
+}
+
+public final class FeedPresenter {
+    let feedLoadingView: FeedLoadingView
+    
+    init(feedLoadingView: FeedLoadingView) {
+        self.feedLoadingView = feedLoadingView
+    }
+    
+    public func didStartLoadingFeed() {
+        feedLoadingView.loadingStateChanged(viewModel: FeedLoadingViewModel(isLoading: true))
+    }
+}
+
+
 final class FeedPresenterTests: XCTestCase {
     func test_init_doesNotSendAnyMessageOnInit() {
-        let sut = SpyView()
+        let (_, view) = makeSUT()
         
-        XCTAssertEqual(sut.messages, [])
+        XCTAssertEqual(view.messages, [])
+    }
+    
+    func test_didStartLoadingFeed_willStartLoadingFeed() {
+        let (sut, view) = makeSUT()
+        
+        sut.didStartLoadingFeed()
+        
+        XCTAssertEqual(view.messages, [.loadingStateChanged(.init(isLoading: true))])
     }
 }
 
 #if DEBUG
 private extension FeedPresenterTests {
-    enum Messages: Equatable {
-        
+    
+    func makeSUT() -> (FeedPresenter, SpyView) {
+        let view = SpyView()
+        let sut = FeedPresenter(feedLoadingView: view)
+        addTrackForMemoryLeak(object: view)
+        addTrackForMemoryLeak(object: sut)
+        return(sut, view)
     }
-    class SpyView {
+    
+    enum Messages: Equatable {
+        case loadingStateChanged(FeedLoadingViewModel)
+    }
+    class SpyView: FeedLoadingView {
+        
+        func loadingStateChanged(viewModel: FeedLoadingViewModel) {
+            messages.append(.loadingStateChanged(viewModel))
+        }
+        
         var messages = [Messages]()
     }
 }
