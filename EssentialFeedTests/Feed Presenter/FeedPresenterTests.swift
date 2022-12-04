87338fe8ce8d036ba73379bac2953522197f280a
Amir Hossein Baradaran Toutounchi <amirhossein.baradarantutunchi@adevinta.com>
2022-12-04 14:32:22 +0100

    add functionality of "didLoadedFeeds"

--- EssentialFeedTests/Feed Presenter/FeedPresenterTests.swift
+++ EssentialFeedTests/Feed Presenter/FeedPresenterTests.swift
@@ -1,4 +1,5 @@
 import XCTest
+import EssentialFeed
 
 struct FeedLoadingViewModel: Equatable {
     let isLoading: Bool
@@ -7,15 +8,29 @@ protocol FeedLoadingView {
     func loadingStateChanged(viewModel: FeedLoadingViewModel)
 }
 
+struct FeedViewModel: Equatable {
+    let feeds: [FeedImage]
+}
+protocol FeedView {
+    func display(viewModel: FeedViewModel)
+}
+
 public final class FeedPresenter {
     let feedLoadingView: FeedLoadingView
-    
-    init(feedLoadingView: FeedLoadingView) {
+    let feedView: FeedView
+
+    init(feedLoadingView: FeedLoadingView, feedView: FeedView) {
         self.feedLoadingView = feedLoadingView
+        self.feedView = feedView
     }
     
     public func didStartLoadingFeed() {
         feedLoadingView.loadingStateChanged(viewModel: FeedLoadingViewModel(isLoading: true))
+    }
+    
+    public func didLoadedFeeds(feeds: [FeedImage]) {
+        feedView.display(viewModel: FeedViewModel(feeds: feeds))
+        feedLoadingView.loadingStateChanged(viewModel: FeedLoadingViewModel(isLoading: false))
     }
 }
 
@@ -34,6 +49,14 @@ final class FeedPresenterTests: XCTestCase {
         
         XCTAssertEqual(view.messages, [.loadingStateChanged(.init(isLoading: true))])
     }
+    
+    func test_didLoadedFeed_displayFeedsAndEndFeedLoading() {
+        let (sut, view) = makeSUT()
+        let anyFeed = UniqueItems()
+        sut.didLoadedFeeds(feeds: anyFeed.models)
+        
+        XCTAssertEqual(view.messages, [.display(.init(feeds: anyFeed.models)), .loadingStateChanged(.init(isLoading: false))])
+    }
 }
 
 #if DEBUG
@@ -41,7 +64,7 @@ private extension FeedPresenterTests {
     
     func makeSUT() -> (FeedPresenter, SpyView) {
         let view = SpyView()
-        let sut = FeedPresenter(feedLoadingView: view)
+        let sut = FeedPresenter(feedLoadingView: view, feedView: view)
         addTrackForMemoryLeak(object: view)
         addTrackForMemoryLeak(object: sut)
         return(sut, view)
@@ -49,8 +72,14 @@ private extension FeedPresenterTests {
     
     enum Messages: Equatable {
         case loadingStateChanged(FeedLoadingViewModel)
+        case display(FeedViewModel)
     }
-    class SpyView: FeedLoadingView {
+    
+    class SpyView: FeedLoadingView, FeedView {
+        func display(viewModel: FeedViewModel) {
+            messages.append(.display(viewModel))
+        }
+        
         
         func loadingStateChanged(viewModel: FeedLoadingViewModel) {
             messages.append(.loadingStateChanged(viewModel))
