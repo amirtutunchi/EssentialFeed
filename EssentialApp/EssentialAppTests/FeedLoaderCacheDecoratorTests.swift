c6bc63b625a47474907f17b9b6dfebf810845b08
Amir Hossein Baradaran Toutounchi <amirhossein.baradarantutunchi@adevinta.com>
2022-12-30 23:43:47 +0100

    refactor  FeedLoaderCacheDecorator to have a factory method

--- EssentialApp/EssentialAppTests/FeedLoaderCacheDecoratorTests.swift
+++ EssentialApp/EssentialAppTests/FeedLoaderCacheDecoratorTests.swift
@@ -18,20 +18,24 @@ final class FeedLoaderCacheDecoratorTests: XCTestCase, FeedLoaderTestCase {
     
     func test_load_succeedOnReturningData() {
         let feed = UniqueFeed()
-        let loader = FeedLoaderStub(result: .success([feed]))
-        let sut = FeedLoaderCacheDecorator(decoratee: loader)
+        let sut = makeSUT(result: .success([feed]))
         expect(sut, toCompleteWith: .success([feed]))
     }
     
     func test_load_returnsErrorOnFailure() {
-        let loader = FeedLoaderStub(result: .failure(anyError()))
-        let sut = FeedLoaderCacheDecorator(decoratee: loader)
+        let sut = makeSUT(result: .failure(anyError()))
         expect(sut, toCompleteWith: .failure(anyError()))
     }
 }
 
 #if DEBUG
 extension FeedLoaderCacheDecoratorTests {
-    
+    private func makeSUT(result: FeedLoader.Result, file: StaticString = #file, line: UInt = #line) -> FeedLoader {
+        let loader = FeedLoaderStub(result: result)
+        let sut = FeedLoaderCacheDecorator(decoratee: loader)
+        addTrackForMemoryLeak(object: loader)
+        addTrackForMemoryLeak(object: sut)
+        return sut
+    }
 }
 #endif
