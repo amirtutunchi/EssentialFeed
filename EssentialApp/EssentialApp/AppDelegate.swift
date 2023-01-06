edc895db38d0a1cb59cc14fd067a3ed0cf7c2fe4
Amir Hossein Baradaran Toutounchi <amirhossein.baradarantutunchi@adevinta.com>
2023-01-06 17:13:43 +0100

    remove "Main" storyboard

--- EssentialApp/EssentialApp/AppDelegate.swift
+++ EssentialApp/EssentialApp/AppDelegate.swift
@@ -8,17 +8,5 @@
 import UIKit
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-    
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        return true
-    }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        let configuration = UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-        return configuration
-    }
-}
+class AppDelegate: UIResponder, UIApplicationDelegate { }
 
--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -6,6 +6,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
     let fileURL: URL = .documentsDirectory
+    
     private lazy var httpClient: HTTPClient = {
         let session = URLSession(configuration: .ephemeral)
         return URLSessionHTTPClient(session: session)
@@ -17,9 +18,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: scene)
         configureWindow()
     }
+    
     func configureWindow() {
         let url = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
         let remoteFeedLoader = RemoteFeedLoader(url: url, client: httpClient)
@@ -39,5 +42,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             imageLoader: imageLoader
         ))
         window?.rootViewController = feedViewController
+        window?.makeKeyAndVisible()
     }
 }
